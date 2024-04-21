5fe1c472005eb490b46f275693567ef0984260b3
Davichobits <davidruizdeveloper@gmail.com>
2024-04-20 21:45:02 -0500

    First commit

---
+++ variables_constantes.dart
@@ -0,0 +1,11 @@
+void main() {
+//   var myName = 'David';
+//   String myName = 'David';
+//   const nyName = 'David';
+
+  late final myName = 'David';
+
+  print('Hola $myName');
+  print('Hola ${myName.toUpperCase()}');
+  print('Hola ${1 + 1}');
+}
