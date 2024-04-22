22d8a20a35ce8bb6f8e0411dcaff9b4cc1edfc00
Davichobits <davidruizdeveloper@gmail.com>
2024-04-22 11:24:23 -0500

    Const y Final

--- 01_variables_y_constantes.dart
+++ 01_variables_y_constantes.dart
@@ -5,6 +5,12 @@ void main() {
 
   late final myName = 'David';
 
+  final List<String> nombresList = ['Maria', 'Juan'];
+  const List<String> nombresConst = ['Maria', 'Juan'];
+
+  nombresList.add('Pedro');
+  // nombresConst.add('Pedro'); // no se puede hacer porque nombresConst es una constante
+
   print('Hola $myName');
   print('Hola ${myName.toUpperCase()}');
   print('Hola ${1 + 1}');
