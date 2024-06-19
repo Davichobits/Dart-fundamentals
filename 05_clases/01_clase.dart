13380d6079419ac86cbbad0dbf00abb286a6a345
Davichobits <davidruizdeveloper@gmail.com>
2024-06-19 11:31:45 -0500

    Creación de una propiedad privada

--- 05_clases/01_clase.dart
+++ 05_clases/01_clase.dart
@@ -4,13 +4,14 @@ void main() {
   Persona persona = new Persona();
   persona
     ..nombre = 'David'
-    ..edad = 33
-    ..bio = 'Programador';
+    ..edad = 33;
+  // ..bio = 'Programador'; // privado
 
   // Lo anterior es igual a lo siguiente:
 
-    // persona.nombre = 'David';
-    // persona.edad = 33;
-    // persona.bio = 'Programador';
+  // persona.nombre = 'David';
+  // persona.edad = 33;
+  // persona.bio = 'Programador';
   print(persona);
+  
 }
--- 05_clases/clases/Persona.dart
+++ 05_clases/clases/Persona.dart
@@ -2,13 +2,13 @@ class Persona {
   // Propiedades
   String? nombre;
   int? edad;
-  String? bio;
+  String? _bio = 'Hola, soy una propiedad privada'; // Con el _ se crea una propiedad privada. Solo puede ser accedida desde la clase.
 
   // Get y Sets
   // Constructores
   // Métodos
   @override
   String toString(){
-    return '$nombre $edad $bio';
+    return '$nombre $edad $_bio';
   }
 }
